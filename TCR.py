a92763a25d73706bcfc577c2e53b292648fa04f1
vauxhall <zero.griffin@gmail.com>
2019-02-18 15:50:48 +0100

    tcr passed

--- TCR.py
+++ TCR.py
@@ -2,11 +2,11 @@ import os
 import unittest
 
 def tcr():
-	os.system('pwd')
 	suite = unittest.defaultTestLoader.discover(".")
 	if unittest.TextTestRunner(verbosity=0, failfast=True).run(suite).wasSuccessful():
 		os.system("git add .")
 		os.system("git commit -m 'tcr passed'")
 	else:
 		os.system("git reset --hard")
+
 tcr()
