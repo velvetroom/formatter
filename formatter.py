2110c5946fa690c1b285652d85c503f59ee7b2f1
vauxhall <zero.griffin@gmail.com>
2019-02-20 15:47:23 +0100

    tcr passed

--- formatter.py
+++ formatter.py
@@ -2,14 +2,26 @@ import sys
 import os
 
 def format(directory):
-	for r, d, f in os.walk(directory):
-		for file in f:
-			if file.endswith(".swift"):
-				print(r + "/" + file)
+	printed = False
+	for r, d, n in os.walk(directory):
+		for i in n:
+			if i.endswith(".swift"):
+				print(r + "/" + i)
+				printed = True
+				fp = open(r + "/" + i)
+				for l in fp.read().split(":"):
+					print(l)
+					break
+# 					if len(l) > 0: 
+# 						print(len(1))
+# 					print("\n")
+				fp.close()
+				break
+		if printed:
+			break
 
 if __name__ == '__main__':
 	format(sys.argv[1])
-
 
 def fibonacci(num):
 	return num + 1
