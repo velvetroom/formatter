412467c5bf98612a9b01498df687ce44dbd0e89f
vauxhall <zero.griffin@gmail.com>
2019-02-20 15:29:27 +0100

    tcr passed

--- formatter.py
+++ formatter.py
@@ -5,7 +5,7 @@ def format(directory):
 	for r, d, f in os.walk(directory):
 		for file in f:
 			if file.endswith(".swift"):
-				print(file)
+				print(r + "/" + file)
 
 if __name__ == '__main__':
 	format(sys.argv[1])
